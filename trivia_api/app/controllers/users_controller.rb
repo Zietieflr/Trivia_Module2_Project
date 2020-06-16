1d3152cebbbf228cf558e0e4d3c9ff04d03b6ef3
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-06-16 17:11:25 -0600

    User page with scorecards and dropdown friends list

--- trivia_api/app/controllers/users_controller.rb
+++ trivia_api/app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users, include: :scorecards 
+    render json: @users, include: [:scorecards, :friends] 
   end
 
   def show
-    render json: @user, include: :scorecards
+    render json: @user, include: [:scorecards, :friends]
   end
 
   def create
--- trivia_app/user.js
+++ trivia_app/user.js
@@ -1,2 +1,80 @@
+const searchParams = new URLSearchParams(window.location.search)
+const id = searchParams.get('userId')
+
+fetch(`http://localhost:3000/users/${id}`)  
+    .then(response => response.json())
+    .then(result => nameAndCards(result))
 
 
+
+function nameAndCards(result){
+    renderUserInfo(result.username, result.scorecards)
+    renderScorecardInfo(result.scorecards)
+    renderDifficulty(result.scorecards)
+    renderCorrect(result.scorecards)
+    displayScorecards(result.scorecards)
+}
+
+function displayScorecards(scorecards) {
+    const scorecard_div = document.getElementById('scorecards')
+    console.log(scorecard_div)
+    scorecards.forEach(scorecard => {showScoreCards(scorecard, scorecard_div)})
+}
+
+function showScoreCards(scorecard, scorecard_div) {
+        console.log(scorecard_div)
+        const fullScorecard = document.createElement('ul')
+        fullScorecard.append(renderScorecardInfo(scorecard.category), renderDifficulty(scorecard.difficulty), renderCorrect(scorecard.correct))
+        scorecard_div.append(fullScorecard)
+}
+
+
+const showUser = document.getElementById('show-user')
+const showScorecards = document.getElementById('scorecard-list')
+const showDifficulty = document.getElementById('difficulty')
+const showCorrect = document.getElementById('percent')
+const showFullCard = document.getElementById('full-card')
+
+
+function renderUserInfo(username){
+    const nameElement = document.createElement('h1')
+    nameElement.innerText = username
+    showUser.append(nameElement)
+}
+
+function renderScorecardInfo(category){
+        const scorecardList = document.createElement('li')
+        scorecardList.classList.add('category')
+        scorecardList.innerText = category
+        return scorecardList    
+}
+
+function renderDifficulty(difficulty){
+        const scorecardDifficulty = document.createElement('li')
+        scorecardDifficulty.classList.add('difficulty')
+        scorecardDifficulty.innerText = difficulty
+        return scorecardDifficulty
+}
+
+function renderCorrect(correct){
+        const scorecardCorrect = document.createElement('li')
+        scorecardCorrect.classList.add('correct')
+        scorecardCorrect.innerText = correct
+        return scorecardCorrect
+}
+
+
+fetch(`http://localhost:3000/users/${id}`)
+    .then(response => response.json())
+    .then(results => friendsListOptions(results))
+
+function friendsListOptions(results){
+    // console.log(results)
+    const select = document.getElementById('Friends-List')
+    results.friends.forEach(friend => {
+        const option = document.createElement('option')
+        option.innerHTML = `<a href='http://localhost:3000/users/${friend.id}'>${friend.username}</a>`
+        select.append(option)
+        console.log(select)
+    })
+}
