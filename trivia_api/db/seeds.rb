395f6df6beccd9c5c996ea581b92f9ffb8ff8d54
Zietieflr <lrmcguire93@gmail.com>
2020-06-16 09:05:19 -0600

    Add friends join table.

--- trivia_api/db/seeds.rb
+++ trivia_api/db/seeds.rb
@@ -6,11 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Friend.destroy_all
 Scorecard.destroy_all
 User.destroy_all
 
 austin = User.create(username: "AwesomeA", password: "aGoodOne")
 logan = User.create(username: "LaconicL", password: "aBadOne")
+un1 = User.create(username: "UN1", password: "aBadOne")
+un2 = User.create(username: "UN2", password: "aBadOne")
+un3 = User.create(username: "UN3", password: "aBadOne")
 
 Scorecard.create(category: "Music", difficulty: "Medium", correct: 0.89, user: austin)
 Scorecard.create(category: "Music", difficulty: "Hard", correct: 0.56, user: austin)
@@ -20,3 +24,11 @@ Scorecard.create(category: "Stuff", difficulty: "Medium", correct: 0.78, user: l
 Scorecard.create(category: "Dragons", difficulty: "Hard", correct: 0.30, user: logan)
 Scorecard.create(category: "Something Else", difficulty: "Easy", correct: 0.22, user: logan)
 Scorecard.create(category: "DONE", difficulty: "Easy", correct: 0.90, user: logan)
+
+Friend.create(friend: logan, friender: austin)
+Friend.create(friend: austin, friender: logan)
+Friend.create(friend: un1, friender: logan)
+Friend.create(friend: un2, friender: austin)
+Friend.create(friend: austin, friender: un1)
+Friend.create(friend: austin, friender: un2)
+Friend.create(friend: austin, friender: un3)
