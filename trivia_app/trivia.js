1fe6b3f226b2b2a532a892730a9655b9234504c5
Zietieflr <lrmcguire93@gmail.com>
2020-06-16 17:34:27 -0600

    Generate categories list. Add category  name to document.

--- trivia_app/trivia.js
+++ trivia_app/trivia.js
@@ -5,10 +5,10 @@ const category = searchParams.get('category')
 const difficulty = searchParams.get('difficulty')
 
 let triviaURL = `https://opentdb.com/api.php?amount=${amount}`
-if (!category === 'any') {
+if (category != 'any') {
   triviaURL = `${triviaURL}&category=${category}`
 }
-if (!difficulty === 'any') {
+if (difficulty != 'any') {
   triviaURL = `${triviaURL}&difficulty=${difficulty}`
 }
 const finalURL = `${triviaURL}&type=multiple`
@@ -16,11 +16,14 @@ const apiURL = `http://localhost:3000/users/${user}`
 
 
 // console.log('url', triviaURL)
-// console.log('final url', finalURL)
+console.log('final url', finalURL)
 // console.log('difficulty', difficulty)
 
 // const testURL = 'https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=multiple'
 // const testBaseURL = 'https://opentdb.com/api.php?amount=10'
+fetch('https://opentdb.com/api_category.php')
+  .then(response => response.json())
+  .then(results => useableCategories(results))
 
 fetch(finalURL)
   .then(response => response.json())
@@ -29,6 +32,25 @@ fetch(finalURL)
 fetch(apiURL)
   .then(response => response.json())
   .then(result => populateUserInfo(result))
+
+function useableCategories(categories) {
+  const selectedCategory = thisCategory(categories)
+  changeCategoryName(selectedCategory)
+}
+
+function thisCategory(categories) {
+  if (category != 'any') {
+    return categories.trivia_categories[category].name
+  } else {
+    const any = 'All categories'
+    return any
+  }
+}
+
+function changeCategoryName(categoryText) {
+  const $category = document.getElementById('category')
+  $category.innerText = categoryText
+}
 
 function populateUserInfo(user) {
   username(user.username) 
@@ -39,7 +61,6 @@ function username(name) {
   $username.innerText = name 
 }
 
-function populateFormInfo(trivia) {
-  const $category = document.getElementById('category')
-  $category.innerText = trivia[0].category 
+function populateTriviaInfo(trivia) {
+  
 }
